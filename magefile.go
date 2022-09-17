d39280b96c48c17e1bb110f15b61e99ee7986c69
Lucas Ramage <lucas.ramage@infinite-omicron.com>
2022-09-17 10:49:51 -0400

    Create magefile.go

---
+++ magefile.go
@@ -0,0 +1,14 @@
+//go:build mage
+
+package main
+
+import (
+    "github.com/magefile/mage/sh"
+)
+
+func Build() error {
+    if err := sh.Run("go", "mod", "download"); err != nil {
+        return err
+    }
+    return sh.Run("go", "install", "./...")
+}
